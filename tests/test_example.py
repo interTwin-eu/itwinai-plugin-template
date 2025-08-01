3bb787ce96eae6505e3acb7b607b11662afb0229
Matteo Bunino <matteo.bunino@gmail.com>
2025-08-01 16:55:00 +0200

    update container build and test

--- tests/test_example.py
+++ tests/test_example.py
@@ -8,6 +8,17 @@ Run the tests from the root of the repository with:
 import pytest
 
 
+# Test plugin import
+def test_plugin_imports():
+    from itwinai.plugins.my_awesome_plugin.another_plugin_subfolder.yet_another_module import (
+        yet_another_function,
+    )
+    from itwinai.plugins.my_awesome_plugin.awesome_module import AwesomeClass, awesome_function
+    from itwinai.plugins.my_awesome_plugin.plugin_subfolder.another_module import (
+        another_function,
+    )
+
+
 # Basic test cases
 def test_foo():
     """Test if 'foo' is a string."""
